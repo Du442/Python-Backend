12f45941a26b4c08e2bd81ac14dcf4673e33b03b
Du442 <eneto7938@gmail.com>
2026-06-02 13:27:10 -0300

    function to switch state

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -2,21 +2,25 @@ class Restaurante:
     restaurantes = []
 
     def __init__(self, nome, categoria):  
-        self.nome = nome
-        self.categoria = categoria
+        self._nome = nome.title()
+        self._categoria = categoria.upper()
         self._ativo = False
         Restaurante.restaurantes.append(self)
 
     def __str__(self):
         return f"{self.nome} | {self.categoria} | {self.ativo}".ljust(50)
     
-    def listar_restaurantes():
-        for i in Restaurante.restaurantes:
-            print(f'{i.nome.ljust(25)} | {i.categoria.ljust(25)} | {i.ativo.ljust(25)}')
+    @classmethod
+    def listar_restaurantes(cls):
+        for i in cls.restaurantes:
+            print(f'{i._nome.ljust(25)} | {i._categoria.ljust(25)} | {i.ativo.ljust(25)}')
 
     @property
     def ativo(self):
         return "☑" if self._ativo else "☐"
+
+    def alternar_estado(self):
+        self._ativo = not self._ativo
 
 restaurante_praca = Restaurante("Praça", "Gourmet")
 restaurante_pizza = Restaurante("Pizza", "Italiana")
